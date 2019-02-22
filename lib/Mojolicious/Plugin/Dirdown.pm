15d396ea094314cbb5a8cdb60b7038d5a6081b71
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2019-02-22 18:57:48 +0100

    Add Dirdown Mojolicious plugin

---
+++ lib/Mojolicious/Plugin/Dirdown.pm
@@ -0,0 +1,16 @@
+package Mojolicious::Plugin::Dirdown;
+use Mojo::Base 'Mojolicious::Plugin', -signatures;
+
+sub register ($self, $app, $conf) {
+
+    # Prepare content directory (may be undef, will be set later)
+    $ENV{DIRDOWN_CONTENT} //= $conf->{dir};
+
+    # Mount the dirdown app in the app using us as a plugin
+    $app->plugin(Mount => {
+        $conf->{prefix} // '/dirdown',
+        $app->home->rel_file('script/dirdown'),
+    });
+}
+
+1;
